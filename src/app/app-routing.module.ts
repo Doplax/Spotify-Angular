9ed71b94a490f6bee92d9b88a29ee0ea122d1824
Doplax <doplax@gmail.com>
2024-02-25 11:53:07 +0100

    feat(routes): LazyLoading implementation in home.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [{
-  path: ''
+  path: '',
+  loadChildren:() => import('./modules/home/home.module').then((m) =>m.HomeModule)
 }];
 
 @NgModule({
