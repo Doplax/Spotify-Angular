ed682779c82c100529c0fe7f5a2528d073f4d271
Doplax <doplax@gmail.com>
2024-05-20 18:57:52 +0200

    Feat(card-player): adds appImgBroken directive

---
+++ src/app/shared/directives/img-broken.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appImgBroken]'
+})
+
+export class ImgBrokenDirective {
+
+  // Cuando una directiva detecte que su host (en este caso una imagen) dispara el evento de error,
+  // cambiaremos su src, por una imagen por defecto
+  @HostListener('error') handleError(): void{
+    const elNative = this.elHost.nativeElement
+    elNative.src = 'https://raw.githubusercontent.com/Doplax/doplax/main/assets/img/product/defaultImage.png'
+  }
+
+  constructor(private elHost: ElementRef) { }
+
+}
