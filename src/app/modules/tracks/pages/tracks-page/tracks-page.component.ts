284d339059ee8326fc1c50b3f5309f916e5020a3
Doplax <doplax@gmail.com>
2024-06-03 11:37:47 +0200

    feat(HttpClientModule): Implementation of the HttpClientModule to fetch the songs to the backend.

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,52 +1,28 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import * as rawData from '@data/tracks.json';
 import { TrackModel } from '@core/models/tracks.model';
 import { TrackService } from 'src/app/tracks/services/track.service';
 import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-tracks-page', // Selector for the component
-  templateUrl: './tracks-page.component.html', // Template file
-  styleUrl: './tracks-page.component.scss', // Stylesheet file
+  selector: 'app-tracks-page',
+  templateUrl: './tracks-page.component.html',
+  styleUrl: './tracks-page.component.scss',
 })
 export class TracksPageComponent implements OnInit, OnDestroy {
-  // Array to hold trending tracks
   public tracksTrending: Array<TrackModel> = [];
-  // Array to hold random tracks
   public tracksRandom: Array<TrackModel> = [];
 
-  // Array to hold subscriptions to observables
   listObservers$: Array<Subscription> = [];
 
-  // Inject TrackService into the component
   constructor(private tracksService: TrackService) {}
 
-  // Lifecycle hook that is called after the component's view has been initialized
   ngOnInit(): void {
-    // Subscribe to the trending tracks observable and update `tracksTrending` and `tracksRandom` when new data arrives
-    const observer1$ = this.tracksService.dataTracksTrending$.subscribe(
-      (response) => {
-        this.tracksTrending = response;
-        this.tracksRandom = response;
-        console.log('Canciones trending =>', response); // Log the trending tracks to the console
-      }
-    );
-
-    // Subscribe to the random tracks observable and update `tracksTrending` with concatenated data
-    const observer2$ = this.tracksService.dataTracksRandom$.subscribe(
-      (response) => {
-        this.tracksTrending = [...this.tracksRandom, ...response]; // Concatenate the current and new tracks
-        console.log('Canciones Random =>', response); // Log the random tracks to the console
-      }
-    );
-
-    // Store the subscriptions in `listObservers$`
-    this.listObservers$ = [observer1$, observer2$];
+    this.tracksService.getAllTracks$().subscribe(
+      response => { console.log(response) }
+    )
   }
 
-  // Lifecycle hook that is called before the component is destroyed
   ngOnDestroy(): void {
-    // Unsubscribe from all observables to prevent memory leaks
     this.listObservers$.forEach((u) => u.unsubscribe());
   }
 }
--- src/app/tracks/services/track.service.ts
+++ src/app/tracks/services/track.service.ts
@@ -1,41 +1,23 @@
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { Observable, of } from 'rxjs';
-import * as dataRaw from '@data/tracks.json';
 
-// Marks this class as available to be injected as a dependency
 @Injectable({
-  providedIn: 'root', // Indicates that this service should be provided at the root level
+  providedIn: 'root',
 })
 export class TrackService {
-  // Observable to hold trending tracks data
-  dataTracksTrending$: Observable<TrackModel[]> = of([]);
-  // Observable to hold random tracks data
-  dataTracksRandom$: Observable<TrackModel[]> = of([]);
 
-  constructor() {
-    // Extract data from the JSON file
-    const { data }: any = (dataRaw as any).default;
+  private readonly URL = environment.api;
 
-    // Initialize the trending tracks observable with data from the JSON file
-    this.dataTracksTrending$ = of(data);
+  constructor(private httpClient: HttpClient) {
 
-    // Create a new observable for random tracks data
-    this.dataTracksRandom$ = new Observable((observer) => {
-      // Example track to emit
-      const trackExample: TrackModel = {
-        _id: 0,
-        name: 'Example',
-        album: 'Cartel de santa',
-        url: 'http://',
-        cover:
-          'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQTsDoLohEwNc7BXvuTBYxezL1ZEZqG0GaxyEcPIc7UJfbLXwSg',
-      };
 
-      // Emit the example track after a delay of 3500 milliseconds
-      setTimeout(() => {
-        observer.next([trackExample]);
-      }, 3500);
-    });
+  }
+
+
+  getAllTracks$(): Observable<any> {
+    return this.httpClient.get(`${this.URL}/tracks`)
   }
 }
