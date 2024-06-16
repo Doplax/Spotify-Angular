0a910a0a61b5d75951ef97232c5e67deb0b50849
Doplax <doplax@gmail.com>
2024-06-16 17:45:23 +0200

    feat: saves the response token in a cookie

--- src/app/modules/auth/pages/login-page/login-page.component.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,37 +1,57 @@
-import { FormControl, FormGroup, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 //import { AuthService } from '@modules/auth/services/auth.service';
 //import { CookieService } from 'ngx-cookie-service';
-import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+  styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
-  formLogin: FormGroup = new FormGroup({})
+  errorSession: boolean = false;
+  formLogin: FormGroup = new FormGroup({});
 
-  constructor(private authService:AuthService ) { }
+  constructor(private authService: AuthService, private cookie:CookieService) {}
 
   ngOnInit(): void {
     this.formLogin = new FormGroup({
-      email: new FormControl('test@prueba.com',[ // Para usar uno valor por defecto
+      email: new FormControl('test@prueba.com', [
+        // Para usar uno valor por defecto
         Validators.required,
-        Validators.email
+        Validators.email,
       ]),
-      password: new FormControl('FakePass',[
+      password: new FormControl('FakePass', [
         Validators.required,
         Validators.minLength(6),
-        Validators.maxLength(12)
+        Validators.maxLength(12),
       ]),
-    })
+    });
   }
 
-  sendLogin():void {
-    const { email , password } = this.formLogin.value;
+  sendLogin(): void {
+    const { email, password } = this.formLogin.value;
 
-    this.authService.sendCredentials(email , password );
+    this.authService.sendCredentials(email, password).subscribe(
+      (responseOk) => {
+        //TODO: When response is Ok
+        console.log("Login successful");
+        const { tokenSession } = responseOk
+        this.cookie.set('token', tokenSession , 4, '/' )
+      },
+      (err) => {
+        //TODO error >= 400
+        this.errorSession = err;
+        console.log('Login failed');
+      }
+    );
   }
 }
--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,13 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly URL = environment.api;
+  constructor(private http: HttpClient,  private cookie:CookieService) {}
 
-  constructor() { }
+  sendCredentials(email: string, password: string): Observable<any> {
+    const body = {
+      email,
+      password,
+    };
 
-  sendCredentials(email: string, password: string): void {
-    console.log(email, password);
+    return this.http.post(`${this.URL}/auth/login`, body).pipe(
+      tap((responseOk: any) => {
+        const { tokenSession } = responseOk;
+        this.cookie.set('token_service', tokenSession, 4, '/');
+      })
+    );
   }
 }
