8c554e83ce5d0cfea207ea7fc113827091366487
Doplax <doplax@gmail.com>
2024-06-16 21:42:38 +0200

    feat: adds SessionGuard to protect HomePageComponent

---
+++ src/app/core/guards/session.guard.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SessionGuard implements CanActivate {
+  constructor(private cookieService: CookieService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.checkCookieSeession();
+  }
+
+  checkCookieSeession(): boolean {
+    try {
+      const token: boolean = this.cookieService.check('token'); //
+      console.log('OK', token);
+      if (!token) {
+        this.router.navigate(['/','auth'])
+      }
+      return token
+
+    } catch (e) {
+      console.log('Algo Sucedio ??', e);
+      return false;
+    }
+  }
+}
