37f546f31db71a32f8e67974cf1c34e01f2f4a3c
Doplax <doplax@gmail.com>
2025-01-01 18:47:06 +0100

    refactor(styles): Refactored sidebar styles to use SCSS nesting

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -30,7 +30,7 @@ export class SideBarComponent implements OnInit {
       {
         name: 'Home',
         icon: 'uil uil-estate',
-        router: ['/', 'auth'],
+        router: ['/'],
       },
       {
         name: 'Buscar',
