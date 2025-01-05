fc73eda9d91dac61012248083894bc7be57e18eb
Doplax <doplax@gmail.com>
2025-01-05 13:12:28 +0100

    feat: pass tracksList as an input to play-list-body component

--- src/app/modules/favorites/pages/favorite-page/favorite-page.component.ts
+++ src/app/modules/favorites/pages/favorite-page/favorite-page.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { TrackService } from 'src/app/tracks/services/track.service';
 
 @Component({
   selector: 'app-favorite-page',
   templateUrl: './favorite-page.component.html',
-  styleUrl: './favorite-page.component.scss'
+  styleUrl: './favorite-page.component.scss',
 })
-export class FavoritePageComponent {
+export class FavoritePageComponent implements OnInit {
+  tracksList: Array<TrackModel> = [];
 
+  constructor(private trackService: TrackService) {
+    this.loadData();
+  }
+
+  ngOnInit(): void {}
+
+  async loadData(): Promise<any> {
+    this.tracksList = await this.trackService.getAllTracks$().toPromise();
+    console.log(this.tracksList);
+  }
 }
