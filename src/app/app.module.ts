00412857314ac0f4cae099270c961f6a28042f1c
Doplax <doplax@gmail.com>
2025-02-05 20:52:03 +0100

    chore: upgrade Angular CLI from v17 to v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,35 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
 import { InjectSessionInterceptor } from '@core/interceptors/inject-sesion.interceptor';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 //import { MatIconModule } from '@angular/material/icon';
 
-@NgModule({
-  declarations: [
-    // Could be: Components, directives, pipes
-    AppComponent,
-  ],
-  imports: [
-    // Only import other Modules
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HeaderComponent,
-    //MatIconModule,
-],
-  providers: [
-    CookieService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: InjectSessionInterceptor,
-      multi: true,
-    },
-    provideAnimationsAsync(),
-  ],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        // Could be: Components, directives, pipes
+        AppComponent,
+    ],
+    bootstrap: [AppComponent], imports: [
+        // Only import other Modules
+        BrowserModule,
+        AppRoutingModule,
+        HeaderComponent], providers: [
+        CookieService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: InjectSessionInterceptor,
+            multi: true,
+        },
+        provideAnimationsAsync(),
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule {}
--- src/app/modules/tracks/tracks.module.ts
+++ src/app/modules/tracks/tracks.module.ts
@@ -4,19 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TracksRoutingModule } from './tracks-routing.module';
 import { TracksPageComponent } from './pages/tracks-page/tracks-page.component';
 import { SharedModule } from '@shared/shared.module'
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    TracksPageComponent,
-
-  ],
-  imports: [
-    CommonModule,
-    TracksRoutingModule,
-    HttpClientModule,
-    SharedModule,
-
-  ]
-})
+@NgModule({ declarations: [
+        TracksPageComponent,
+    ], imports: [CommonModule,
+        TracksRoutingModule,
+        SharedModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class TracksModule { }
