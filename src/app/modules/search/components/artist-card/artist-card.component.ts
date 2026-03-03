1644864d5caa1926b1026894af67af2692385990
Doplax <doplax@gmail.com>
2026-03-03 12:19:25 +0100

    feat(artist-card): Updates the playback logic and improves the component design

--- src/app/modules/search/components/artist-card/artist-card.component.ts
+++ src/app/modules/search/components/artist-card/artist-card.component.ts
@@ -9,21 +9,26 @@ import { MultimediaService } from '@shared/services/multimedia.service';
   standalone: false
 })
 export class ArtistCardComponent implements OnInit {
-  @Input() artistData!: TrackModel;
+  /** First track whose artist info drives the card display */
+  @Input() firstTrack!: TrackModel;
 
   public avatar: string = '';
-  public name: string = '';
   public artistName: string = '';
+  public isPlaying: boolean = false;
 
   constructor(public multimediaService: MultimediaService) {}
 
   ngOnInit(): void {
-    this.avatar = this.artistData.cover;
-    this.name = this.artistData.name;
-    this.artistName = this.artistData.artist?.name ?? '';
+    this.avatar = this.firstTrack.cover;
+    this.artistName = this.firstTrack.artist?.name ?? this.firstTrack.name;
+
+    this.multimediaService.trackInfo$.subscribe((track) => {
+      this.isPlaying =
+        !!track && track.artist?.name === this.artistName;
+    });
   }
 
   play(): void {
-    this.multimediaService.trackInfo$.next(this.artistData);
+    this.multimediaService.trackInfo$.next(this.firstTrack);
   }
 }
